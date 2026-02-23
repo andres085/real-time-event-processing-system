596012dd15656588334e6522ef0647133e9c35b2
andres085 <andresito.martinez1985@gmail.com>
2026-02-23 10:06:14 -0300

    Arch refactor and creation of api module with the migrations

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,11 +9,7 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
-	router.NotFound = http.HandlerFunc(app.notFoundResponse)
-	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
-
-	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/ingest", app.ingestHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/api/healthcheck", app.healthcheckHandler)
 
 	return router
 }
--- migrations/000001_create_raw_events_table.up.sql
+++ migrations/000001_create_raw_events_table.up.sql
@@ -1,15 +1,15 @@
-CREATE TABLE raw_events (
-    id BIGSERIAL,
+CREATE TABLE IF NOT EXISTS raw_events (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     timestamp TIMESTAMPTZ NOT NULL,
     source VARCHAR(100) NOT NULL,
     method VARCHAR(10) NOT NULL,
     endpoint VARCHAR(255) NOT NULL,
     status_code INTEGER NOT NULL,
     response_time_ms INTEGER NOT NULL,
-    request_size_bytes INTEGER DEFAULT 0,
-    response_size_bytes INTEGER DEFAULT 0,
+    request_size_bytes INTEGER NOT NULL,
+    response_size_bytes INTEGER NOT NULL,
     user_agent TEXT,
     ip_address INET,
-    processed BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMPTZ DEFAULT NOW()
+    processed BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 );
